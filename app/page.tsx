e4b4554ada2cef9626028e322c9feef8f0ee6f52
Waleedkhani255 <201253134+WaleedAfridi-1@users.noreply.github.com>
2026-04-07 22:48:45 +0500

    feat: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold text-gray-700">Home Page</h1>
+    </div>
+  );
+}
