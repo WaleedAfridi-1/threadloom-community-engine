b856150c0b6cb7f5900105d13e7529544a6cfca7
Waleed Afridi <patlu7150@gmail.com>
2026-04-24 02:38:38 +0500

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,11 @@
+import { PrismaClient } from "@prisma/client";
 
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client";
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const connectionString = `${process.env.DATABASE_URL}`;
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient();
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
+if (process.env.NODE_ENV !== "production")
+  globalForPrisma.prisma = prisma;
